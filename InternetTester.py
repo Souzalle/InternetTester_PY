b4fe984f55e5045592dd795f7dc8881335904fe4
Alexandre Souza <alexandre.souzafilho@hotmail.com>
2025-06-15 14:40:02 -0300

    Added the functions ( still not working normally)

--- InternetTester.py
+++ InternetTester.py
@@ -33,18 +33,44 @@ nome_label.pack(side="left")
 frameConteudo = ttk.Frame(janela)
 frameConteudo.pack(pady=(20,20))
 
+
+
+#função velocidade
+def calcular_speed():
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    download_speed = st.download / 1000000 #Mbps
+    upload_speed = st.upload() / 1000000 #Mbps
+
+    return download_speed, upload_speed
+
+
+# função inicializar o texto
+
+def start_test():
+    info_label.config(text="Testando...")
+    download_speed, upload_speed = calcular_speed()
+
+    # Atualizar as variaveis de velocidade
+    speed_var["download"].set(f"{download_speed: .2f} Mbps")
+    speed_var["upload"].set(f"{upload_speed: .2f} Mbps")
+
+
+
+
+
 info_label = ttk.Label(frameConteudo, text="Pressione o botão para testar a velocidade da internet")
 info_label.grid(row=0, column=0, columnspan=3, pady=(0, 10))
 
 download_label = ttk.Label(frameConteudo, text="Velocidade de Download: ")
 download_label.grid(row=1, column=0, sticky='w', pady=(0, 5))
 
-Upload_label = ttk.Label(frameConteudo, text="Velocidade de Upload: ")
-Upload_label.grid(row=2, column=0, sticky='w', pady=(0, 5))
+upload_label = ttk.Label(frameConteudo, text="Velocidade de Upload: ")
+upload_label.grid(row=2, column=0, sticky='w', pady=(0, 5))
 
 speed_var = {
     'download': tk.StringVar(),
-    'Upload': tk.StringVar(),
+    'upload': tk.StringVar(),
 }
 
 
@@ -56,7 +82,7 @@ rapido_imagem = Image.open(rapido_image_local)
 normal_image = Image.open(normal_image_local)
 lento_image = Image.open(lento_image_local)
 
-iniciar_botão = ttk.Button(frameConteudo, text="Iniciar Teste", )
+iniciar_botão = ttk.Button(frameConteudo, text="Iniciar Teste", command= start_test)
 iniciar_botão.grid(row=3, column=0, columnspan=3, pady=(20, 0))
 
 
